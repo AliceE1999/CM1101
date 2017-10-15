d6ec2ba521efe99532b5e199c1e940d860475c74
BuildTools <ieuangriffiths1230@gmail.com>
2017-10-15 17:42:07 +0100

    Revert "Covfefe"
    This reverts commit a1ff1c08ca8138093d7ad1a12a46db59990ca2de, reversing
    changes made to 2dfcd604d11a1159bfe1c11513d8d317c747123b.
    
    I messed up the command

--- hello.py
+++ hello.py
@@ -3,10 +3,6 @@ print("Changes in another")
 print("World")
 print("More changes")
 print("Still more changes")
-<<<<<<< HEAD
 print("Is that phone in your pocket?")
 print("Because your ass is calling me ;)")
 
-=======
-print("Your iron, you smell")
->>>>>>> d610bceb32ab40c643562256ae9707252f12e4e2
